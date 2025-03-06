dae2133a9e86ae9930d4ff7230383ec27ef452de
HHY <HHY@DESKTOP-9N7P36V>
2025-03-06 23:35:29 +0900

    2.결합(coupling)

--- src/main/java/com/in28minute/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/in28minute/learn_spring_framework/AppGamingBasicJava.java
@@ -2,13 +2,24 @@ package com.in28minute.learn_spring_framework;
 
 import com.in28minute.learn_spring_framework.game.GameRunner;
 import com.in28minute.learn_spring_framework.game.MarioGame;
+import com.in28minute.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		
-		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame);
+		/* 1. 
+		 * 
+		 * var marioGame = new MarioGame();
+		 * 
+		 * var gameRunner = new GameRunner(marioGame); 
+		 * gameRunner.run();
+		 */
+		
+		
+		var superContraGame = new SuperContraGame();
+		
+		var gameRunner = new GameRunner(superContraGame);
 		gameRunner.run();
 
 	}
--- src/main/java/com/in28minute/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minute/learn_spring_framework/game/GameRunner.java
@@ -1,9 +1,19 @@
 package com.in28minute.learn_spring_framework.game;
 
 public class GameRunner {
-	MarioGame game;
 	
-	public GameRunner(MarioGame game) {
+	/* 1. 게임 러너는 마리오 게임과 강하게 결합되어있다. 다른게임을 하려면 게임을 바꿔줘야됨
+	 * 결합이란 무언가를 변경하는 데 얼마나 많은 작업이 관련되어 있는지에 대한 측정이다.
+	 * 
+	 * private MarioGame game;
+	 * 
+	 * public GameRunner(MarioGame game) { this.game = game; }
+	 */
+	
+	
+	private SuperContraGame game;
+	
+	public GameRunner(SuperContraGame game) {
 		this.game = game;
 	}
 
